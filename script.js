4f23b1b4481a155bd86bfee53ba73fe5bf164bfd
fathyElgazar <fathyelgazar348@gmail.com>
2023-10-25 06:41:41 +0300

    Make basic math functions

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict";
+// basic functions
+function add(firstNum, secondNum) {
+  return firstNum + secondNum;
+}
+function subtract(firstNum, secondNum) {
+  return firstNum - secondNum;
+}
+function multiply(firstNum, secondNum) {
+  return firstNum * secondNum;
+}
+function divide(firstNum, secondNum) {
+  return firstNum / secondNum;
+}
+
+let numberOne = 2;
+let operator = `+`;
+let numberTwo = 3;
+
+function operate(firstNum, secondNum) {
+  return multiply(firstNum, secondNum);
+}
